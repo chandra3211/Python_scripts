fe5b9b3046796214f90c4d20b0de1f6ea06de22c
chandra <sekhar.chandra529@gmail.com>
2023-06-20 19:23:55 +0530

    first commit

---
+++ myscript.py
@@ -0,0 +1,24 @@
+'''Variable - a variable is a container (storage area) to hold data. we use the assignment operator = to assign a value to a variable.
+we can assign multiple variables'''
+a, b, c = 5, 3.2, 'Hello'
+
+print(a,b,c)
+
+#output - 5 3.2 Hello
+
+'''If we want to assign the same value to multiple variables at once, we can do this as:'''
+a = b = 5
+print(a)
+print(b)
+
+output:
+5
+5
+below are the conditions to write variables:
+1. A variable name must start with a letter or underscore character(a-z) (_).
+2. A variable name cant start with number.
+3. A variable names are only have alpha-numeric characters and underscores (A-z, 0-9, _). (special characters are not allowed).
+4. variable names are case-sensitive (name, Name, NAME are three different variabes).
+5. variable name should not match with python keyword.
+
+
